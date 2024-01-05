ab16c337381b3398020b4e0ac1a83a277207fc65
Silkou Maxim <maxim.silkou.2002@gmail.com>
2024-01-05 14:02:43 +0300

    add bll

---
+++ StudyMaterialLibrary.BLL/Services/StudyFileService.cs
@@ -0,0 +1,27 @@
+﻿using StudyLibrary.Application.Abstractions;
+using StudyLibrary.Application.ViewModels.StudyFile;
+
+namespace StudyLibrary.Application.Services;
+
+public class StudyFileService : IStudyFileService
+{
+    public StudyFileService()
+    {
+
+    }
+
+    public Task AddFile(AddStudyFileViewModel studyFile)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteFile(Guid fileId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateFile(Guid fileId, AddStudyFileViewModel studyFile)
+    {
+        throw new NotImplementedException();
+    }
+}
